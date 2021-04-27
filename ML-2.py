e500b64210044ca32ed24a3b2044622539f1d28b
zahradehghanpur <zahradehghanpour>
2021-04-27 15:25:34 +0430

    change ?

--- ML-2.py
+++ ML-2.py
@@ -1,48 +1,53 @@
 from pandas import read_csv
 import seaborn as sns
 from matplotlib import pyplot
-
-
+import pandas as pd 
+import matplotlib.pyplot as plt
+from pandas.plotting import scatter_matrix
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn import datasets
+from sklearn.decomposition import PCA
 path = r".\imports-85.data"
-headernames = ['symboling','normalized-losses','make','num-of-doors','body-style','drive-wheels',
-'engine-location','wheel-base','length','width','height','curb-weight','engine-type','num-of-cylinders',
-'engine-size','fuel-system','bore','stroke','compression-ratio','horsepower','peak-rpm',
-'city-mpg','highway-mpg','price']
+headernames = ['symboling', 'normalized-losses', 'make', 'num-of-doors', 'body-style', 'drive-wheels',
+               'engine-location', 'wheel-base', 'length', 'width', 'height', 'curb-weight', 'engine-type', 'num-of-cylinders',
+               'engine-size', 'fuel-system', 'bore', 'stroke', 'compression-ratio', 'horsepower', 'peak-rpm',
+               'city-mpg', 'highway-mpg', 'price']
 data = read_csv(path, names=headernames)
 print(data.head(50))
 
-#### shape of data
-print("shape of data")
-print(data.shape)
+# # shape of data
+# print("shape of data")
+# print(data.shape)
 
-#### class distribution
-print("class distribution")
-for headername in headernames:
-    count_class = data.groupby(headername).size()
-    print(count_class)
+# # class distribution
+# print("class distribution")
+# for headername in headernames:
+#     count_class = data.groupby(headername).size()
+#     print(count_class)
 
-#### features types
-print("features types")
-print(data.dtypes)
+# # features types
+# print("features types")
+# print(data.dtypes)
 
-### features correlations
-print("features correlations")
-correlations = data.corr(method='pearson')
-print(correlations)
+# # features correlations
+# print("features correlations")
+# correlations = data.corr(method='pearson')
+# print(correlations)
 
-### feature scatter plot
+# # feature scatter plot
 
-print("feature scatter plot")
+# print("feature scatter plot")
+# scatter_matrix(data)
+# plt.show()
 
-
-
-### density diagram
-data.plot(kind='density', subplots=True, layout=(3,3), sharex=False)
-pyplot.show()
-
-
+# # density diagram
+# data.plot(kind='density', subplots=True, layout=(3, 3), sharex=False)
+# pyplot.show()
 
 
+iris = datasets.load_iris()
+X = iris.data[:, :2]  # we only take the first two features.
+y = iris.target
 
-
-
+print(X)
---
+++ ML-4.py
@@ -0,0 +1,38 @@
+from sklearn.cluster import KMeans
+from pandas import read_csv
+from sklearn import preprocessing
+
+
+path = r".\imports-85.data"
+headernames = ['symboling' , 'normalized-losses', 'make','fuel-type', 'aspiration', 'num-of-doors', 'body-style', 'drive-wheels',
+               'engine-location', 'wheel-base', 'length', 'width', 'height', 'curb-weight', 'engine-type', 'num-of-cylinders',
+               'engine-size', 'fuel-system', 'bore', 'stroke', 'compression-ratio', 'horsepower', 'peak-rpm',
+               'city-mpg', 'highway-mpg', 'price']
+dataset = read_csv(path, names=headernames)
+number = preprocessing.LabelEncoder()
+dataset['make'] = number.fit_transform(dataset['make'])
+dataset['fuel-type'] = number.fit_transform(dataset['fuel-type'])
+dataset['aspiration'] = number.fit_transform(dataset.aspiration)
+dataset['num-of-doors'] = number.fit_transform(dataset['num-of-doors'])
+dataset['body-style'] = number.fit_transform(dataset['body-style'])
+dataset['drive-wheels'] = number.fit_transform(dataset['drive-wheels'])
+dataset['engine-location'] = number.fit_transform(dataset['engine-location'])
+dataset['engine-type'] = number.fit_transform(dataset['engine-type'])
+dataset['num-of-cylinders'] = number.fit_transform(dataset['num-of-cylinders'])
+dataset['fuel-system'] = number.fit_transform(dataset['fuel-system'])
+
+
+
+# dataset['engine-size'] = number.fit_transform(dataset.engine-size)
+dataset=dataset.fillna(-999)
+X = dataset.drop('price', axis=1)
+Y = dataset['price']
+
+kmeans = KMeans(n_clusters = 3)
+kmeans.fit(X)
+y_kmeans = kmeans.predict(X)
+
+plt.scatter(X[:, 0], X[:, 1], c = y_kmeans, s = 20, cmap = 'summer')
+centers = kmeans.cluster_centers_
+plt.scatter(centers[:, 0], centers[:, 1], c = 'blue', s = 100, alpha = 0.9);
+plt.show()
