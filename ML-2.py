a07f6c9f34a4cad7c7cbc7a4253fc3b4761cc2c6
pouyehbnj <pouyehbnj@gmail.com>
2021-04-27 13:51:54 +0430

    scatter diagram added

--- ML-2.py
+++ ML-2.py
@@ -1,48 +1,42 @@
 from pandas import read_csv
 import seaborn as sns
 from matplotlib import pyplot
-
-
+import pandas as pd 
+import matplotlib.pyplot as plt
+from pandas.plotting import scatter_matrix
 path = r".\imports-85.data"
-headernames = ['symboling','normalized-losses','make','num-of-doors','body-style','drive-wheels',
-'engine-location','wheel-base','length','width','height','curb-weight','engine-type','num-of-cylinders',
-'engine-size','fuel-system','bore','stroke','compression-ratio','horsepower','peak-rpm',
-'city-mpg','highway-mpg','price']
+headernames = ['symboling', 'normalized-losses', 'make', 'num-of-doors', 'body-style', 'drive-wheels',
+               'engine-location', 'wheel-base', 'length', 'width', 'height', 'curb-weight', 'engine-type', 'num-of-cylinders',
+               'engine-size', 'fuel-system', 'bore', 'stroke', 'compression-ratio', 'horsepower', 'peak-rpm',
+               'city-mpg', 'highway-mpg', 'price']
 data = read_csv(path, names=headernames)
 print(data.head(50))
 
-#### shape of data
+# shape of data
 print("shape of data")
 print(data.shape)
 
-#### class distribution
+# class distribution
 print("class distribution")
 for headername in headernames:
     count_class = data.groupby(headername).size()
     print(count_class)
 
-#### features types
+# features types
 print("features types")
 print(data.dtypes)
 
-### features correlations
+# features correlations
 print("features correlations")
 correlations = data.corr(method='pearson')
 print(correlations)
 
-### feature scatter plot
+# feature scatter plot
 
 print("feature scatter plot")
+scatter_matrix(data)
+plt.show()
 
-
-
-### density diagram
-data.plot(kind='density', subplots=True, layout=(3,3), sharex=False)
+# density diagram
+data.plot(kind='density', subplots=True, layout=(3, 3), sharex=False)
 pyplot.show()
-
-
-
-
-
-
-
