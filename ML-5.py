0d03c1008b5f1d8bedb2eee1aadae7ec64a4bcbd
zahradehghanpur <zahradehghanpour>
2021-05-02 12:33:36 +0430

    regression completed

--- ML-5.py
+++ ML-5.py
@@ -10,6 +10,14 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 from sklearn.preprocessing import PolynomialFeatures
+import numpy as np
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+
+
+
+
 
 path = r".\imports-85.data"
 headernames = ['symboling' , 'normalized-losses', 'make','fuel-type', 'aspiration', 'num-of-doors', 'body-style', 'drive-wheels',
@@ -17,52 +25,60 @@ headernames = ['symboling' , 'normalized-losses', 'make','fuel-type', 'aspiratio
                'engine-size', 'fuel-system', 'bore', 'stroke', 'compression-ratio', 'horsepower', 'peak-rpm',
                'city-mpg', 'highway-mpg', 'price']
 dataset = read_csv(path, names=headernames)
-number = preprocessing.LabelEncoder()
-dataset['make'] = number.fit_transform(dataset['make'])
-dataset['fuel-type'] = number.fit_transform(dataset['fuel-type'])
-dataset['aspiration'] = number.fit_transform(dataset.aspiration)
-dataset['num-of-doors'] = number.fit_transform(dataset['num-of-doors'])
-dataset['body-style'] = number.fit_transform(dataset['body-style'])
-dataset['drive-wheels'] = number.fit_transform(dataset['drive-wheels'])
-dataset['engine-location'] = number.fit_transform(dataset['engine-location'])
-dataset['engine-type'] = number.fit_transform(dataset['engine-type'])
-dataset['num-of-cylinders'] = number.fit_transform(dataset['num-of-cylinders'])
-dataset['fuel-system'] = number.fit_transform(dataset['fuel-system'])
+# number = preprocessing.LabelEncoder()
+# dataset['make'] = number.fit_transform(dataset['make'])
+# dataset['fuel-type'] = number.fit_transform(dataset['fuel-type'])
+# dataset['aspiration'] = number.fit_transform(dataset.aspiration)
+# dataset['num-of-doors'] = number.fit_transform(dataset['num-of-doors'])
+# dataset['body-style'] = number.fit_transform(dataset['body-style'])
+# dataset['drive-wheels'] = number.fit_transform(dataset['drive-wheels'])
+# dataset['engine-location'] = number.fit_transform(dataset['engine-location'])
+# dataset['engine-type'] = number.fit_transform(dataset['engine-type'])
+# dataset['num-of-cylinders'] = number.fit_transform(dataset['num-of-cylinders'])
+# dataset['fuel-system'] = number.fit_transform(dataset['fuel-system'])
 
 
 
 # dataset['engine-size'] = number.fit_transform(dataset.engine-size)
-dataset=dataset.fillna(-999)
-X = dataset.drop('price', axis=1)
-Y = dataset['price']
+# dataset=dataset.fillna(-999)
+X = np.array(dataset['highway-mpg']).reshape(-1, 1)
+Y = np.array(dataset['curb-weight']).reshape(-1, 1)
 
 
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.20)
-classifier = DecisionTreeClassifier()
-classifier.fit(X_train, y_train)
-# y_pred = classifier.predict(X_test)
-# ### confusion_matrix
-# result = confusion_matrix(y_test, y_pred)
-# print(result)
+
 regressor = LinearRegression()
 
 regressor.fit(X_train, y_train)
-print(regressor.intercept_)
 
 y_pred = regressor.predict(X_test)
-print(y_pred)
+
+plt.scatter(X_test, y_test,color='g')
+plt.plot(X_test, y_pred,color='k')
+
+plt.show()
+
+rms = sqrt(mean_squared_error(y_pred,y_test))
+
+
+print(f'rms in linear is {rms}')
 
 poly_reg = PolynomialFeatures(degree=4)
 X_poly = poly_reg.fit_transform(X)
 pol_reg = LinearRegression()
 pol_reg.fit(X_poly, Y)
-# dataset.plot(x='make', y='price', style='o')
-# plt.title('Hours vs Percentage')
-# plt.xlabel('Hours Studied')
-# plt.ylabel('Percentage Score')
-# plt.show()
 
-plt.scatter(X_poly, Y,color='g')
-plt.plot(X, pol_reg.predict(X),color='k')
+y_pred = pol_reg.predict(X_poly)
+# plt.figure(figsize=(10,8))
+# plt.scatter(X, Y)
+# plt.plot(X, y_pred)
+# print(r2_score(Y, y_pred))
+# plt.show()
+plt.scatter(X, Y,color='g')
+plt.plot(X, y_pred,color='k')
 
 plt.show()
+
+rms = mean_squared_error(Y, y_pred, squared=False)
+
+print(f'rms in polynomial is {rms}')
